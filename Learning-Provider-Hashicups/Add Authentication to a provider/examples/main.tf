05a0e08a40e9f329a16ba80cb13eb9a801cafb76
amanborkar99 <61972395+amanborkar99@users.noreply.github.com>
2021-04-19 10:06:17 +0530

    Added Authentication in provider

---
+++ Learning-Provider-Hashicups/Add Authentication to a provider/examples/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    hashicups = {
+      version = "0.2"
+      source  = "hashicorp.com/edu/hashicups"
+    }
+  }
+}
+
+provider "hashicups" {
+  username = "education"
+  password = "test123"
+}
+
+
+module "psl" {
+  source = "./coffee"
+
+  coffee_name = "Packer Spiced Latte"
+}
+
+output "psl" {
+  value = module.psl.coffee
+}
