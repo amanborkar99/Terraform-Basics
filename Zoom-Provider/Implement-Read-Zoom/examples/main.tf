68ec32fcdc3362b67b0760ea3b46a9e65bf0abe0
amanborkar99 <61972395+amanborkar99@users.noreply.github.com>
2021-04-23 09:16:09 +0530

    Removed token

--- Zoom-Provider/Implement-Read-Zoom/examples/main.tf
+++ Zoom-Provider/Implement-Read-Zoom/examples/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "zoom" {
-  token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6ImxOR0pCSGp1Uk9PRktDTTY4TGpIMGciLCJleHAiOjE2MTkxMDE5NjksImlhdCI6MTYxOTA5NjU2OX0.WjPXHfy4bhZpcPdSPa0U33mLARQqobaoifGs03m_4Jk"
+  token = "Bearer Token"
 }
 
 data "zoom_corp_data" "xyz"{
